508ac1c7654ccf643c99627c2a24a8c1f97a5c31
pablo <pablo@pabloestrada.us>
2012-08-02 21:54:47 -0700

    Added datamodel, converted to use pointers

---
+++ datamodel.h
@@ -0,0 +1,22 @@
+#ifndef datamodel_h
+#define datamodel_h
+
+#include <WString.h>
+
+typedef struct {
+  String route;
+  String route_direction;
+  String stop_description;
+  
+  int in_stop_ID;
+//  time_t in_last_update;
+  char prediction_time[3][2];
+  String in_prediction_URL;
+  
+  int out_stop_ID;
+//  time_t out_last_update;
+  String out_prediction_URL;
+  long last_attempt;
+} prediction;
+
+#endif 
--- muniticker.ino
+++ muniticker.ino
@@ -7,87 +7,33 @@
  created 18 Dec 2009
  by David A. Mellis
  */
-//////////////////////////////////////////////
-// Get XML formatted data from the web.
-// 1/6/08 Bob S. - Created
-//
-//  Assumptions: single XML line looks like:
-//    <tag>data</tag> or <tag>data
-//
-// Get current weather observation for Raleigh from weather.gov in XML format
-//
-//////////////////////////////////////////////
 
 #include <SPI.h>
 #include <Ethernet.h>
 #include <String.h>
-//#include <Time.h>
-
+#include "datamodel.h"
 
 // Max String length may have to be adjusted depending on data to be extracted
 #define MAX_STRING_LEN  250
 #define MAX_STRING_ROWS 50
 
-// Church st station outbound: stop ID = 6998
-// Church st station inbound: stop ID = 5726
-
-// N outbound stop at Church and Duboce: stop ID = 4447
-// N inbound stop at Church and Duboce: stop ID = 4448
-
-// J outbound stop at Church and 14th: stop ID = 7316
-// J inbound stop at Church and Duboce: stop ID = 4006
-
-// F outbound stop at Church and 14th: stop ID = 5661
-// F inbound stop at Church and Market: stop ID = 5662
-
-/* ////// table to hold prediction times \\\\\\\\
-
-_________________|_______|_________|_____________|______________|______________|______________|________________
-stop_description | route | stop_ID | last_update | prediction_1 | prediction_2 | prediction_3 | prediction_URL
-
-struct to hold it:
-
-struct prediction {
-  String route;
-  char direction[3];
-  String stop_description;
-  
-  int in_stop_ID;
-  time_t in_last_update;
-  int in_prediction_1;
-  int in_prediction_2;
-  int in_prediction_3;
-  String in_prediction_URL;
-  
-  int out_stop_ID;
-  time_t out_last_update;
-  int out_prediction_1;
-  int out_prediction_2;
-  int out_prediction_3;
-  String out_prediction_URL;
-}
-
-struct prediciton F, J, K, L, M, N, six, twentytwo, seventyone;
-
-   ////// table to hold prediction times \\\\\\\\ */
-
 // Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
 byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-byte ip[] = { 192,168,1,177 };
-byte server[] = { 74,125,39,104 }; // NextMUNI
+//byte ip[] = { 192,168,1,177 };
+IPAddress ip(192,168,1,177);
+IPAddress myDns(8,8,8,8);
 byte gateway[] = { 192,168,1,176 }; // my macbook, sharing its internet connection
-//byte nextmuni[] = { 64,124,123,57 }; // nextmuni API
-byte nextmuni[] = { 192,168,1,176 }; // nextmuni API
+byte subnet[] = { 255,255,255,0 };
+//byte nextmuni[] = { 64,124,123,57 }; // nextmuni API, IP address resolved by webservices.nextbus.com
+byte nextmuni[] = { 192,168,1,176 }; // nextmuni API simulated by local computer MAMP
+//char nextmuni[] = "webservices.nextbus.com";
 
 char tagStr[MAX_STRING_LEN] = "";
-//char dataStr[][MAX_STRING_LEN] = { "" };
 char* dataStr = { "" };
 char tmpStr[MAX_STRING_LEN] = "";
 
 String tempRow;
 
-//char endTag[3] = {'<', '/', '\0'};
 char endTag[3] = {'/', '>', '\0'};
 char LF=10;
 char CR=13;
@@ -95,63 +41,33 @@ char LT=62; // > char
 int len;
 int rowCounter = 0;
 
-const int request_interval = 15000;  // delay between requests, 15 seconds
+const int request_interval = 10000;  // delay between requests, 60 seconds
 const int refresh_interval = 1000;  // delay between screen refreshes, 1 second
-
 long last_refreshed = 0;            // last time text was written to display
 
-typedef struct {
-  String route;
-  String route_direction;
-  String stop_description;
-  
-  int in_stop_ID;
-//  time_t in_last_update;
-  char prediction[3][2];
-  String in_prediction_URL;
-  
-  int out_stop_ID;
-//  time_t out_last_update;
-  String out_prediction_URL;
-  long last_attempt;
-} prediction;
-
-prediction F, J, K, L, M, N, six, twentytwo, seventyone;
+prediction N, F, J, K, L, M, six, twentytwo, seventyone;
+prediction* F_ptr = &F; prediction* J_ptr = &J; prediction* K_ptr = &K;
+prediction* L_ptr = &L; prediction* M_ptr = &M; prediction* N_ptr = &N;
+prediction* six_ptr = &six; prediction* twentytwo_ptr = &twentytwo; prediction* seventyone_ptr = &seventyone;
 
 int num_predictions = 0;
 int attempt_connect = 1;
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server 
-// that you want to connect to (port 80 is default for HTTP):
-// EthernetClient client(nextmuni, 80);
 EthernetClient client;
 
 void setup() {
-  // start the Ethernet connection:
-  Ethernet.begin(mac, ip, gateway);
-  // start the serial library:
+  Ethernet.begin(mac, ip, myDns, gateway, subnet); // start the Ethernet connection:
   Serial.begin(9600);
-  // give the Ethernet shield a second to initialize:
-  delay(800);
+  delay(800); // give the Ethernet shield a second to initialize
 }
 
 void loop()
 {
+// find time of last update for the route, if needed, re-connect to the URL for the route, extraction some number of predictions
+// store predictions and timestamp of update into memory, search for any special messages, move to the next route
 
-// find time of last update for the route
-// if enough time has elapsed, process the route
-// connect to the URL for the route
-// extraction some number of predictions
-// store predictions and timestamp of update into memory
-// search for any special messages
-
-// move to the next route
-
-//  Serial.println("OK, ready");
-//  get_update();
   if (attempt_connect) {
-    connect_to_update();
+    connect_to_update('N');
     delay(400);
     attempt_connect = 0;
 //    String URL = URL_constructor(4448,'N');
@@ -159,7 +75,7 @@ void loop()
   
   if (client.connected()) {
     if (client.available()) {
-       serialEvent();
+       serialEvent(N_ptr);
     }
 //    else client.stop();
   } 
@@ -167,19 +83,20 @@ void loop()
   if (!client.connected()) {
     client.flush();
     client.stop();
+//    Serial.println("Client stopped");
 }
   
-  if (millis() - N.last_attempt > request_interval) {
+  if (millis() - N_ptr->last_attempt > request_interval) {
     attempt_connect = 1;
     Serial.println("trigger refresh");
   }
   else if (millis() - last_refreshed > refresh_interval) {
     // reprint previously recorded times
     Serial.println("");
-    Serial.println(N.route + " " + N.route_direction + " ");
+    Serial.println(N_ptr->route + " " + N_ptr->route_direction + " ");
     for (int i=0; i<3;i++) {
       for (int j=0;j<2;j++) {
-         Serial.print(N.prediction[i][j]);
+         Serial.print(N_ptr->prediction_time[i][j]);
       }
       if (i<2) Serial.print(", ");
     }
@@ -187,38 +104,46 @@ void loop()
   }
 }
 
-void connect_to_update() {
+void connect_to_update(char _route) {
   Serial.println("");
   Serial.println("Connecting...");
     // if you get a connection, report back via serial:
   if (client.connect(nextmuni, 80)) {
     Serial.println("Connected");
-    // Make a HTTP request:
-    // client.println("GET /service/publicXMLFeed?command=predictions&a=sf-muni&r=N&s=4448 HTTP/1.0");
-    client.println("GET /munixml.txt HTTP/1.0");
+
+//    client.println("GET /service/publicXMLFeed?command=predictions&a=sf-muni&r=N&s=4448 HTTP/1.0");
+
+// DNS-based request:
+    client.println("GET /N-munixml.txt HTTP/1.0");
+//    client.println("GET /service/publicXMLFeed?command=predictions&a=sf-muni&r=N&s=4448 HTTP/1.0");
+//    client.println("Host: webservices.nextbus.com");
+//    client.println("User-Agent: arduino");
+//    client.println("Accept */*");
+//    client.println("Connection: close");
     client.println();
   }  else {
     Serial.println("Connection failed.");
   }
   
   // record the last time the connection was attempted
-  N.last_attempt = millis();
+  N_ptr->last_attempt = millis();
+  Serial.println("last attempt: " + N_ptr->last_attempt);
 }
 
 
 ////////////////////////////
 // Process each char from web
-void serialEvent() {
+void serialEvent(prediction* _route) {
    char inChar = client.read();
- 
+   Serial.print(inChar);
    if ( (inChar == 10) /* || (inChar == CR) /* || (inChar == LT) */) {
      tempRow = tmpStr;
 //     Serial.println(tempRow);
      if (tempRow.startsWith("<predictions", 0)) {
-       if (N.route.length() < 1) {
+       if (_route->route.length() < 1) {
          int RouteTitleIndexStart = tempRow.indexOf("routeTitle"); //This gives the name of the MUNI line
          int RouteTitleIndexEnd = tempRow.indexOf("routeTag"); //routeTag is the next tag after RouteTitle   
-         N.route = tempRow.substring(RouteTitleIndexStart + 12, RouteTitleIndexEnd - 2);
+         _route->route = tempRow.substring(RouteTitleIndexStart + 12, RouteTitleIndexEnd - 2);
        }
      }
      
@@ -227,16 +152,16 @@ void serialEvent() {
          int predictionValueIndexStart = tempRow.indexOf("minutes");
          int predictionValueIndexEnd = tempRow.indexOf("isDeparture");
          String PredictionValue = tempRow.substring(predictionValueIndexStart + 9, predictionValueIndexEnd - 2);      
-         PredictionValue.toCharArray(N.prediction[num_predictions], 3);
+         PredictionValue.toCharArray(_route->prediction_time[num_predictions], 3);
          num_predictions++;
       }
      }
      
      if (tempRow.startsWith("  <direction", 0)) {
-       if (N.route_direction.length() < 1) {
+       if (_route->route_direction.length() < 1) {
          int directionTitleIndexStart = tempRow.indexOf("title");
          int directionTitleIndexEnd = tempRow.indexOf("to");
-         N.route_direction = tempRow.substring(directionTitleIndexStart + 7, directionTitleIndexEnd - 1);
+         _route->route_direction = tempRow.substring(directionTitleIndexStart + 7, directionTitleIndexEnd - 1);
        }
      }
      
@@ -271,7 +196,22 @@ void addChar (char ch, char* str) {
       str[strlen(str)] = ch;
 }
 
-// sample URL: http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=N&s=4448
+// sample URL: http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=N&s=4448 //
+
+// list of routes for a line: http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r=J //
+
+
+// Church st station outbound: stop ID = 6998
+// Church st station inbound: stop ID = 5726
+
+// N outbound stop at Church and Duboce: stop ID = 4447
+// N inbound stop at Church and Duboce: stop ID = 4448
+
+// J outbound stop at Church and 14th: stop ID = 7316
+// J inbound stop at Church and Duboce: stop ID = 4006
+
+// F outbound stop at Church and 14th: stop ID = 5661
+// F inbound stop at Church and Market: stop ID = 5662
 
 /* ////// example response \\\\\\\
 HTTP/1.1 200 OK
@@ -284,22 +224,18 @@ Connection: close
 Content-Type: text/xml
 
 <?xml version="1.0" encoding="utf-8" ?> 
-<body copyright="All data copyright San Francisco Muni 2011.">
+<body copyright="All data copyright San Francisco Muni 2012.">
 <predictions agencyTitle="San Francisco Muni" routeTitle="N-Judah" routeTag="N" stopTitle="Duboce Ave &amp; Church St" stopTag="4448">
-  <direction title="Inbound to Downtown/Caltrain">
-  <prediction epochTime="1318910429586" seconds="130" minutes="2" isDeparture="false" dirTag="N__IB1" vehicle="1406" block="9715" tripTag="4621621" />
-  <prediction epochTime="1318911298770" seconds="1000" minutes="16" isDeparture="false" dirTag="N__IB1" vehicle="1451" block="9704" tripTag="4621622" />
-  <prediction epochTime="1318911948951" seconds="1650" minutes="27" isDeparture="false" dirTag="N__IB1" vehicle="1416" block="9706" tripTag="4621623" />
-  <prediction epochTime="1318912769827" seconds="2471" minutes="41" isDeparture="false" affectedByLayover="true" dirTag="N__IB1" vehicle="1504" block="9707" tripTag="4621624" />
-  <prediction epochTime="1318914509827" seconds="4211" minutes="70" isDeparture="false" affectedByLayover="true" dirTag="N__IB1" vehicle="1461" block="9711" tripTag="4621626" />
+  <direction title="Inbound to Caltrain via Downtown">
+  <prediction epochTime="1343886965750" seconds="284" minutes="4" isDeparture="false" dirTag="N__IBJU4K" vehicle="1544" block="9725" tripTag="5148882" />
+  <prediction epochTime="1343888237468" seconds="1556" minutes="25" isDeparture="false" affectedByLayover="true" dirTag="N__IB1" vehicle="1513" block="9715" tripTag="5148763" />
+  <prediction epochTime="1343888638996" seconds="1957" minutes="32" isDeparture="false" affectedByLayover="true" dirTag="N__IB1" ve  hicle="1511" block="9704" tripTag="5148764" />
+  <prediction epochTime="1343889287632" seconds="2606" minutes="43" isDeparture="false" affectedByLayover="true" dirTag="N__IB1" vehicle="1539" block="9723" tripTag="5148762" />
+  <prediction epochTime="1343889838996" seconds="3157" minutes="52" isDeparture="false" affectedByLayover="true" dirTag="N__IB1" vehicle="1538" block="9706" tripTag="5148765" />
   </direction>
-  <direction title="Inbound to Downtown/Caltrain (M-F)">
-  <prediction epochTime="1318913452536" seconds="3153" minutes="52" isDeparture="false" affectedByLayover="true" dirTag="N__IBJU4K" vehicle="1489" block="9724" tripTag="4621625" />
-  <prediction epochTime="1318913932536" seconds="3633" minutes="60" isDeparture="false" affectedByLayover="true" dirTag="N__IBJU4K" vehicle="1525" block="9709" tripTag="4621748" />
-  </direction>
-<message text="www.sfmta.com or&#10;311 for Muni info."/>
-<message text="Effective 10/15&#10;N Judah will go&#10;to Caltrain&#10;on weekends"/>
 <message text="PROOF OF PAYMENT&#10;is required when&#10;on a Muni vehicle&#10;or in a station."/>
+<message text="No Elevator at&#10;Embarcadero Sta."/>
+<message text="sfmta.com or 3 1 1&#10;for Muni info"/>
 </predictions>
 </body>
 */
